3e5e319d7e71b78a5617d93fc723b180524bc48b
jonschwadron <jonschwadron@gmail.com>
2015-12-08 23:00:58 -0500

    update

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  db: process.env.db || 'localhost',
+  db: process.env.db || 'heroku_d4m1dfdj:cbu9ao761pm92h2m3g8tfmfhps@ds027415.mongolab.com:27415/heroku_d4m1dfdj',
   clientSecret: process.env.clientSecret || '3153855e6d414ab49fe75cfcdd1d2f35',
   tokenSecret: process.env.tokenSecret || 'diablo'
 };
