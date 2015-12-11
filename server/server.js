049ed72071bfb53ced7b504b028e13885053f201
jonschwadron <jonschwadron@gmail.com>
2015-12-10 19:48:10 -0500

    changed path

--- server/server.js
+++ server/server.js
@@ -80,7 +80,7 @@ if (app.get('env') === 'production') {
     protocol == 'https' ? next() : res.redirect('https://' + req.hostname + req.url);
   });
 }
-app.use(express.static(path.join(__dirname, '../../client')));
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 2628000000 }));
 
 /*
  |--------------------------------------------------------------------------
